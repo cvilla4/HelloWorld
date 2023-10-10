0ecef37ea27c79d22720b9e9013b5b282f23326c
cvilla4 <neymarvilla1@gmail.com>
2023-10-10 14:03:41 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+# Fixed the quotation mark
+print("Python's strings are easy to use")
+# Fixed the quotation mark and added "s" after string
+print('We can even include "quotes" in strings')
+# Fixed the quotation mark
+print("hello"+"world")
+greeting="Hello"
+# Fixed the quotation mark
+name=input("Please enter your name:")
+print(greeting + name)
+# if we want a space, we can add that too
+print(greeting+ ''+name)
